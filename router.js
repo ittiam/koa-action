edfcc39d241463cd9ff9d6ba7efb9108e772dc24
czh <ittiamc@gmail.com>
2018-01-12 13:56:37 +0800

    delete router.js

--- router.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const controller = require('./controllers/index');
-
-module.exports = function (router) {
-  // 首页
-  router.get('/', controller.index);
-  router.post('/create', controller.create);
-  router.get('/user/:name', controller.user);
-};
